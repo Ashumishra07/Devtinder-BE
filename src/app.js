fd173bcbacc1b653ebb8a8fb22be9b6391ce9f87
Ashumishra07 <ashutoshrmishra07@gmail.com>
2025-09-23 21:25:33 +0530

    play with rote and checks order and ordermatters in real,2) Create multiple http method get,post,put,delete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@
 const express = require("express");
 app=express();
 
+// Create multiple http method get,post,put,delete
+app.get("/a",(req,res)=>{
+    res.send("Hello ANd Lets boom it");
+});
+
+app.post("/hello",(req,res)=>{
+    res.send("Hello ANd Lets boom it only hello");
+});
+
+app.put("/xyz",(req,res)=>{
+    res.send("Hello ANd Lets boom it xyz");
+});
+
+app.delete("/hello/2",(req,res)=>{
+    res.send("Hello ANd Lets boom it hello2");
+});
+
+// play with rote and checks order and ordermatters in real 
 app.use("/new",(req,res)=>{
     res.send("Welcome to server at 7777");
     
@@ -11,8 +29,12 @@ app.use("/test",(req,res) =>{
 
     res.send("HEllo welcome to server");
 });
+app.use("/",(req,res)=>{
+    res.send("Welcome to server at 7777 hey");
+    
+});
 
-app.use("/hello",(req,res) => {
+app.use("/hello/2",(req,res) => {
     res.send("Namaste hi hello");
 });
 
