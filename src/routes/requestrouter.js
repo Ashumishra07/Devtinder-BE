0419198c0fd60107ba37c6950d961ac3abe29773
Ashumishra07 <ashutoshrmishra07@gmail.com>
2026-01-18 21:47:31 +0530

    send connection request api added

--- src/routes/requestrouter.js
+++ src/routes/requestrouter.js
@@ -1,6 +1,8 @@
 const { userMiddleware } = require("../middlewares/auth.middleware");
 const express = require('express')
 const requestRouter = express.Router();
+const ConnectionRequest = require("../models/connectionrequest.model");
+const User = require("../models/user");
 
 
 requestRouter.post('/request/send/:status/:toUserId', userMiddleware,async(req,res)=>{
@@ -8,12 +10,54 @@ requestRouter.post('/request/send/:status/:toUserId', userMiddleware,async(req,r
     try{
          const fromUserId =req.user._id;
          const toUserId = req.params.toUserId;
-         const status = req.params.status;
+         const Status = req.params.status;
+         // console.log("??",fromUserId,toUserId,status);
 
-     }
-    
-    catch(error){
-        throw new error
+         const allowedStatuses = ['Interested', 'Ignored', 'Accepted', 'Rejected'];
+         if(!allowedStatuses.includes(Status)){
+            return res.status(400).json({message:"Invalid status"});
+         }
+
+         const toUser = await User.findById(toUserId);
+         if(!toUser){
+            return res.status(404).json({message:"Recipient user not found"});
+         }
+
+         if(fromUserId == toUserId){
+            return res.status(400).json({message:"You cannot send request to yourself"});
+         }
+
+         const existingConnectionRequest = await ConnectionRequest.findOne({
+           $or:[{
+            fromUserId,
+            toUserId
+           },
+           {
+            fromUserId:toUserId,
+            toUserId:fromUserId
+           }] 
+            
+         });
+
+         if(existingConnectionRequest){
+            return res.status(400).json({message:"Request already sent"});
+         }
+      
+         const newConnectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            Status
+         });
+
+        const data = await newConnectionRequest.save();
+
+         res.json({message:"Request sent successfully"},
+         data
+         );       
+         
+     }catch(err){
+        
+        throw new Error("Something went wrong!!!"+err.message);
     }
 });
 
