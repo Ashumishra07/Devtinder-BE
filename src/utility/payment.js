6ac6e9d06898a71f46c2ba0e66c198dc88ef802d
Ashumishra07 <ashutoshrmishra07@gmail.com>
2026-08-05 20:36:33 +0530

    Solve error of key_id

--- src/utility/payment.js
+++ src/utility/payment.js
@@ -1,9 +1,11 @@
 const Razorpay = require('razorpay');
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+dotenv.config();
 
 let razorpayInstance = new Razorpay({
     key_id:process.env.RAZORPAY_API_KEY,
     key_secret:process.env.RAZORPAY_SECRET_KEY,
 });
+console.log(process.env.RAZORPAY_API_KEY, process.env.RAZORPAY_SECRET_KEY);
 
 module.exports ={razorpayInstance};
