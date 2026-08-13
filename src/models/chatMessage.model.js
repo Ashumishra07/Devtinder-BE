ccbd9de371c4e1ff9c3b73a971a14c52ea74dcce
Ashumishra07 <ashutoshrmishra07@gmail.com>
2026-08-13 20:28:43 +0530

    Implemented Real time chat using Websocket- Socket.io

---
+++ src/models/chatMessage.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    senderId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    text:{  
+           type:String,
+           required:true
+    }, 
+        
+},{timestamps:true});
+
+const chatMessageSchema = new mongoose.Schema({
+     participants:[
+        {type: mongoose.Schema.Types.ObjectId, ref:"User"}
+     ],
+
+     messages:[messageSchema]
+});
+
+const Chat = mongoose.model("Chat", chatMessageSchema);
+module.exports = Chat;
